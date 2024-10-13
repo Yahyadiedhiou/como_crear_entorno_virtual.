7728ba5116564eb1eccc0a3e789a354a386b6ced
Yahyadiedhiou <168641174+Yahyadiedhiou@users.noreply.github.com>
2024-10-13 19:46:40 +0200

    entorno virtual creacion
    aqui enseno como crear un entorno virtual , eso es para tener un sitio donde guardar paquetes

---
+++ como crear un entorno virtual.py
@@ -0,0 +1,25 @@
+'''
+Paso 2: Crear un Entorno Virtual en VS Code
+Abre la Terminal Integrada en VS Code:
+Ve a View > Terminal o usa el atajo Ctrl + Shift + en Windows/Linux o Cmd + Shift + en macOS.
+Navega al directorio donde deseas crear tu proyecto.
+
+
+Crear el Entorno Virtual:
+En la terminal, crea el entorno virtual con el siguiente comando:
+bash
+Copiar código "python -m venv venv"
+
+Este comando creará una carpeta venv dentro de tu proyecto que contendrá el entorno virtual.
+#################################################
+
+Paso 3: Activar el Entorno Virtual en VS Code
+Activar el Entorno Virtual desde la Terminal en VS Code:
+En Windows:
+bash
+Copiar código "venv\Scripts\activate"
+En macOS y Linux:
+bash
+Copiar código "source venv/bin/activate"
+
+'''
